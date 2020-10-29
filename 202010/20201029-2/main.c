2aad4af4096ff031375f2dd5f9654e4b9e34b6c6
QiuYeDx <20205900@stu.neu.edu.cn>
2020-10-29 18:37:56 +0800

    -3

--- 202010/20201029-2/main.c
+++ 202010/20201029-2/main.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    int y = 1,n,N,K,i,x = 1;  //n是二进制数的位数,x shi chu shu
+    int y = 2,n,N,K,i,x = 2;  //n是二进制数的位数,x是除数。
     int number[65];
     int remain[65];
 
-    scanf("%d%d",&N,&K);
+    scanf("%d",&N);
     remain[1] = N;
-    for(n=0;!N/y==0;n++){
+    for(n=1;N/y>0;n++){
         y*=2;
     }
 
     for(i=1;i<=n;i++,x*=2) {
-        number[i] = remain[i] % x;
-        remain[i+1] = remain[i] - x;
+        number[i] = (remain[i] % x) * 2 / x;
+        remain[i+1] = remain[i] - number[i]*x/2;
     }
 
-    for(i=1;i<=n;i++)
+    for(i=n;i>=1;i--)
         printf("%d",number[i]);
-    return i;
+    return 0;
 }
---
+++ 202010/20201029-3/cmake-build-debug/CMakeFiles/20201029_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "20201029_3"
+  "20201029_3.pdb"
+  "CMakeFiles/20201029_3.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/20201029_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
