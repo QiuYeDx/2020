a667090b1c4699b42b4d047ec7f2abba57ee6c58
QiuYeDx <20205900@stu.neu.edu.cn>
2020-11-03 17:18:01 +0800

    20201103-8

--- 202011/20201103-8/main.c
+++ 202011/20201103-8/main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
+void test(int _n)
+{
+    int _i,_k=1,_kk;
+    int ys[128]={0};
+    int _sum=0;
+
+    for(_i=1;_i<_n;_i++)
+    {
+        if(_n % _i == 0)
+        {
+            ys[_k] = _i;
+            _k++;
+        }
+    }
+    _k--;
+    for(_kk=1;_kk<=_k;_kk++)
+        _sum += ys[_kk];
+    if(_sum == _n)
+    {
+        printf("%d its factors are", _sum);
+        for(_kk=1;_kk<=_k;_kk++)
+            printf(" %d",ys[_kk]);
+        printf("\n");
+    }
+}
+
 int main() {
-    printf("Hello, World!\n");
+    int N,n;
+
+    scanf("%d",&N);
+    for(n=6;n<=N;n+=2)
+        test(n);
     return 0;
 }
