e2110c922f269121675108ed7cf66d3d8c799b59
QiuYeDx <20205900@stu.neu.edu.cn>
2020-11-03 12:09:02 +0800

    20201103-4

--- 202011/20201103-4/main.c
+++ 202011/20201103-4/main.c
@@ -1,22 +1,16 @@
-#include <stdio.h>
-int pow(int x,int y)
-{
-        int _i=1,_sum=0;
-        while(_i<=y) {
-            _sum *= x;
-            _i++;
-        }
-        return _sum;
-}
+#include<stdio.h>
+#include<math.h>
 
 int main() {
     int n,i,sum=0;
 
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        sum+=2 * pow(10,i-1);
-    }
+    for(;n>0;n--)
+        for(i=1;i<=n;i++)
+        {
+            sum+=2 * (int)pow(10,i-1);
+
+        }
     printf("%d",sum);
 
     return 0;
