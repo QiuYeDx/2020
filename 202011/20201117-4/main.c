241cd9f62edab8aed3ae4d60850a856231e18841
QiuYeDx <20205900@stu.neu.edu.cn>
2020-11-17 13:22:41 +0800

    20201117-4-ok

--- 202011/20201117-4/main.c
+++ 202011/20201117-4/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char word[24][32]={0};
+    int length[24]={0};
+    int i=1,j=1;
+    char g;
+    while(scanf("%c",&g)!=EOF)
+    {
+        if(g==' ')
+        {
+            j=1;
+            i++;
+        }
+        else
+        {
+            word[i][j]=g;
+            length[i]++;
+            j++;
+        }
+    }
+    for(int k=i;k>=1;k--)
+    {
+        for(int n=1;n<=length[k];n++)
+            printf("%c", word[k][n]);
+        if(k>=2)
+            printf("\n");
+    }
     return 0;
 }
