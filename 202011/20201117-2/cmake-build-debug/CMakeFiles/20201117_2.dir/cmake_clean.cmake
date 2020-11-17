0266a0b88ad5245dbe6b7683294a97ff7dc4b85c
QiuYeDx <20205900@stu.neu.edu.cn>
2020-11-17 09:31:40 +0800

    20201117-2

---
+++ 202011/20201117-2/cmake-build-debug/CMakeFiles/20201117_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "20201117_2"
+  "20201117_2.pdb"
+  "CMakeFiles/20201117_2.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/20201117_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
