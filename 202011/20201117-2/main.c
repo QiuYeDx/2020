6d1487397d357e569035b3a9beb79eb768d900f6
QiuYeDx <20205900@stu.neu.edu.cn>
2020-11-17 12:18:33 +0800

    20201117-2-ok

--- 202011/20201117-2/main.c
+++ 202011/20201117-2/main.c
@@ -7,8 +7,8 @@ int i_js=1;
 int i_os=1;
 int n_js=0;
 int n_os=0;
-char js[64]={0};
-char os[64]={0};
+char js[32]={0};
+char os[32]={0};
 int i_output_js=1;
 int i_output_os=1;
 
@@ -17,8 +17,7 @@ int main()
     int j_yn=1;
     char ch;
 
-    scanf("%c",&ch);
-    while((ch=getchar())!=EOF)
+    while(scanf("%c",&ch)!=EOF)
     {
         if(j_yn==1)
         {
