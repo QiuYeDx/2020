f4d95d2d7e0b2a7a46450710d085bcf2cc46e7e1
QiuYeDx <20205900@stu.neu.edu.cn>
2020-11-18 21:41:05 +0800

    20201118-7-2

---
+++ 202011/20201118-7-2/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char ch_a[100]={0};
+    char ch_b[100]={0};
+    int i=0,k=0;
+
+    scanf("%s%s",ch_a,ch_b);
+    printf("%s",ch_a);
+    printf("%s",ch_b);
+    return 0;
+}
