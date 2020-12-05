e3c56a0739e1c3f667baad61f2a64b6bd8e27850
白子涵(秋夜Dx <71062076+QiuYeDx@users.noreply.github.com>
2020-12-05 16:42:20 +0800

    20201205

---
+++ 202012/20201205-2/cmake-build-debug/CMakeFiles/20201205_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "20201205_2.exe"
+  "20201205_2.exe.manifest"
+  "20201205_2.lib"
+  "20201205_2.pdb"
+  "CMakeFiles/20201205_2.dir/main.c.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/20201205_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
