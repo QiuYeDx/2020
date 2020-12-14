a02086255be8c8e1ae87eba9b4630f3b2646dec4
白子涵(秋夜Dx <71062076+QiuYeDx@users.noreply.github.com>
2020-12-14 20:36:53 +0800

    20201214-3-ok~

--- 202012/20201214-3-3/main.c
+++ 202012/20201214-3-3/main.c
@@ -1,5 +1,8 @@
+//C语言第二次作业T3
+//终于过了。。。犯了一些低级错误（s1写成s2、pr++的位置不对等）
+//另外。。算是第一次实践指针的应用吧！这道题用数组写更不容易错，也省的debug了……啊嘞啊嘞~
 #include <stdio.h>
-#include <string.h>
+#include <string.h>   //C
 
 int main() {
     char s1[1002]={0};
@@ -15,24 +18,25 @@ int main() {
     pt=s1;
     while(strstr(pt,s2)!=NULL)
     {
-        p[i]=pt;
         pt=strstr(pt,s2);
+        p[i]=pt;
         pt=pt+length;
         i++;
     }
     i--;
     p[0]=s1;     //从p[0]开始打印字符，直到遇到p[1],然后从p[1]+length开始打印……
     char * pr=p[0];
-    for(k=1;(*pr)!='\0';pr++)
+    for(k=1;(*pr)!='\0';)
     {
-        if(pr==p[k])
+        if(k<=i && pr==p[k])
         {
             putchar('#');
             pr+=length;
             k++;
             continue;
         }
-        scanf("%c",pr);
+        putchar(*pr);
+        pr++;
     }
     return 0;
 }
---
+++ 202012/20201214-4/cmake-build-debug/CMakeFiles/20201214_4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "20201214_4.exe"
+  "20201214_4.exe.manifest"
+  "20201214_4.pdb"
+  "CMakeFiles/20201214_4.dir/main.c.obj"
+  "lib20201214_4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/20201214_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
