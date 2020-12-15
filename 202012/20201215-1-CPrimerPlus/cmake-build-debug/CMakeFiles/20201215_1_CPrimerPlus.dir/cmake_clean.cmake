9bc0b68b8f6704107b0d9e201947bbbcb929325b
白子涵(秋夜Dx <71062076+QiuYeDx@users.noreply.github.com>
2020-12-15 14:01:45 +0800

    20201215-CPP

---
+++ 202012/20201215-1-CPrimerPlus/cmake-build-debug/CMakeFiles/20201215_1_CPrimerPlus.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "20201215_1_CPrimerPlus.exe"
+  "20201215_1_CPrimerPlus.exe.manifest"
+  "20201215_1_CPrimerPlus.pdb"
+  "CMakeFiles/20201215_1_CPrimerPlus.dir/main.c.obj"
+  "lib20201215_1_CPrimerPlus.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/20201215_1_CPrimerPlus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
